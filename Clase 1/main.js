618f8025cc7f589e9934a26a368aef842a526f53
andresf2448 <58791994+andresf2448@users.noreply.github.com>
2023-11-18 13:24:22 -0500

    Clase 1

---
+++ Clase 1/main.js
@@ -0,0 +1,49 @@
+// comentario de una linea
+
+/* 
+comentario
+de
+bloque
+*/
+
+//forma recomendada para declarar variables
+// let edad = 25; //tipo de dato number
+
+// let nombre = "andres"; //tipo de dato string
+
+// let cantidad = "7";
+
+//forma no recomendada para declarar variables
+// var direccion = "AV";
+
+// const NUMERO = 56; //constantes
+
+//para las variables
+
+//camelCase => nombreBonitoGrande
+//PascalCase => NombreBonitoGrande
+//snake_case => nombre_bonito_grande
+//kebab => nombre-bonito-grande
+
+// let numeroUno = 5;
+// let numeroDos = 10;
+
+// let resultadoResta = numeroUno - numeroDos;
+// let resultadoMultiplicacion = numeroUno * numeroDos;
+// let resultadoDivision = numeroUno / numeroDos;
+// let resultadoSuma = numeroUno + numeroDos;
+
+// console.log("resultado de la suma", resultadoSuma);
+// console.log("resultado de la resta", resultadoResta);
+// console.log("resultado de la multiplicacion", resultadoMultiplicacion);
+// console.log("resultado de la division", resultadoDivision);
+
+// let nombre = prompt("Ingrese su nombre");
+
+// alert("Hola cómo estás " + nombre);
+
+// console.log(typeof 6);
+// console.log(typeof "andres");
+
+let edad = Number(prompt("Ingrese su edad"));
+alert(10 + edad);
