8f3740272360301464f25f5696b65e7ce2287165
andresf2448 <andresf2448@gmail.com>
2023-12-23 11:13:25 -0500

    Clase 6

--- Clase 6/parte 1/main.js
+++ Clase 6/parte 1/main.js
@@ -1,5 +1,5 @@
 //EVENTOS
-let boton = document.getElementById("boton");
+// let boton = document.getElementById("boton");
 
 // const respuesta = () => {
 //   console.log("Hola diste click");
@@ -20,3 +20,67 @@ let boton = document.getElementById("boton");
 // boton.addEventListener(evento, respuesta);
 // let nombre = prompt("Ingrese su nombre");
 // boton.addEventListener("click", () => respuesta(nombre));
+
+// let boton = document.getElementById("boton");
+
+//EVENTOS DEL MOUSE
+
+// boton.addEventListener("mousedown", () => console.log("mousedown"));
+// boton.addEventListener("mouseup", () => console.log("mouseup"));
+// boton.addEventListener("click", () => console.log("click"));
+// boton.addEventListener("mouseover", () => console.log("mouseover"));
+// boton.addEventListener("mouseout", () => console.log("mouseout"));
+// boton.addEventListener("mousemove", () => console.log("mousemove"));
+
+// boton.addEventListener("mousedown", () => console.log("mousedown"));
+// boton.addEventListener("mouseup", () => console.log("mouseup"));
+// boton.addEventListener("click", () => console.log("click"));
+
+// let saludo = document.getElementById("saludo");
+
+// boton.addEventListener("mousedown", () => {
+//   saludo.className = "rojo";
+// });
+
+// boton.addEventListener("mouseup", () => {
+//   saludo.className = "verde";
+// });
+
+// boton.addEventListener("mouseover", () => {
+//   saludo.className = "amarillo";
+// });
+
+// boton.addEventListener("mouseout", () => {
+//   saludo.className = "azul";
+// });
+
+//eventos del teclado
+let nombre = document.getElementById("nombre");
+let saludo = document.getElementById("saludo");
+
+// nombre.addEventListener("keydown", () => console.log("keydown"));
+// nombre.addEventListener("keyup", () => console.log("keyup"));
+
+// let saludo = document.getElementById("saludo");
+
+// nombre.addEventListener("keyup", (e) => {
+//   if (e.key === "a") {
+//     saludo.className = "rojo";
+//   } else if (e.key === "s") {
+//     saludo.className = "verde";
+//   } else if (e.key === "d") {
+//     saludo.className = "amarillo";
+//   } else {
+//     saludo.className = "azul";
+//   }
+// });
+
+// nombre.addEventListener("change", () => console.log("change"));
+
+// nombre.addEventListener("input", () => {
+//   if (!nombre.value.includes("@") || !nombre.value.includes(".com")) {
+//     saludo.className = "rojo";
+//   } else {
+//     saludo.className = "blanco";
+//   }
+// });
