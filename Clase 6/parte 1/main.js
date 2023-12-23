132f48f1819ece4800f999b29446ab25f5c6c3a9
andresf2448 <andresf2448@gmail.com>
2023-12-23 10:13:22 -0500

    Clase 6

---
+++ Clase 6/parte 1/main.js
@@ -0,0 +1,22 @@
+//EVENTOS
+let boton = document.getElementById("boton");
+
+// const respuesta = () => {
+//   console.log("Hola diste click");
+// };
+
+// const respuesta = (nombre) => {
+//   alert(`Hola ${nombre} diste click`);
+// };
+
+//1 forma
+//CUIDADO se pasa la declaración de la función
+// boton.addEventListener("click", respuesta);
+
+//2 forma
+// boton.onclick = respuesta;
+
+// let evento = prompt("Ingrese el evento requerido");
+// boton.addEventListener(evento, respuesta);
+// let nombre = prompt("Ingrese su nombre");
+// boton.addEventListener("click", () => respuesta(nombre));
