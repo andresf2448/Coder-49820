a9b21ab21717f3e712afdce92040da25a3091acb
andresf2448 <andresf2448@gmail.com>
2024-01-06 13:16:49 -0500

    Clase 7

---
+++ Clase 7/parte 2/main.js
@@ -0,0 +1,40 @@
+// const nombres = ["juan", "pedro", "matias"];
+// console.log(...nombres);
+// console.log(nombres.join(" "));
+
+// const numeros = [4, 4, 5, 7, 8, 7];
+// console.log(Math.min(...numeros));
+// console.log(Math.max(...numeros));
+
+//paso por valor
+// let nombre1 = "andres";
+// let nombre2 = nombre1;
+
+// nombre2 = "juan";
+// console.log(nombre1);
+
+//paso referencia
+// let persona1 = { nombre: "andres", edad: 56 };
+// let persona2 = {...persona1};
+// persona2.nombre = "juan";
+
+// console.log(persona1);
+
+// const usuario = { nombre: "pepito", edad: 54, direccion: "AV1" };
+// const usuario1 = {
+//   ...usuario,
+//   nombre: "juan",
+//   cabello: "rojo",
+// };
+
+// console.log(usuario1);
+
+// const sumar = (...numeros) => {
+//   console.log(numeros.reduce((acum, item) => acum + item, 0));
+// };
+
+// sumar(4, 5, 67, 8, 9, 6, 54, 33);
+
+// sumar(4, 5, 67);
+
+//REPASO
